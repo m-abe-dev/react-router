3c4073837d259549b5a2603145f4a638f57385cc
m-abe-dev <66056064+m-abe-dev@users.noreply.github.com>
2021-05-16 22:42:21 +0900

    Linkを使わないページ遷移 直接Linkのように遷移するルーティングだけではなく、ボタンのような遷移の時に使う

--- src/pageOne.jsx
+++ src/pageOne.jsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const PageOne = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+  const history = useHistory();
+  const onClickDetailA = () => {
+    history.push("/pageOne/detailA");
+  };
   return (
     <div>
       <h1>page one</h1>
       <Link to={{ pathname: "/pageOne/detailA", state: arr }}>Detail A</Link>
       <br />
       <Link to="/pageOne/detailB">Detail B</Link>
+      <br />
+      <button onClick={onClickDetailA}>Detail A</button>
     </div>
   );
 };
--- src/pageOneDetailA.jsx
+++ src/pageOneDetailA.jsx
@@ -1,11 +1,14 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const PageOneDetailA = () => {
   const { state } = useLocation();
   console.log(state);
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>page one Detail A</h1>
+      <button onClick={onClickBack}>back</button>
     </div>
   );
 };
